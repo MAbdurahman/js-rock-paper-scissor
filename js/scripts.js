fcbde6c8b0803736e41a5dda95cbeaf347f5c9b9
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2026-08-29 05:58:09 -0500

    Complete desktop version and add function updateBlinkingTextState to scripts

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,17 @@
 
 document.addEventListener("DOMContentLoaded", function(){
    console.log("DOM fully loaded and parsed");
+
+   const gameMessage = document.querySelector('#game-message');
+
+   function updateBlinkingState() {
+      const expectedText = 'Let The Game Begin!';
+
+      if (gameMessage.innerText.trim().toLowerCase() === expectedText) {
+         gameMessage.classList.add('blinking-text');
+
+      } else {
+         gameMessage.classList.remove('blinking-text');
+      }
+   }
 });
